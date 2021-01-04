5fd91776155193633d3f590351b2ba55cd8d04fd
yuankui <yuankui@meituan.com>
2021-01-04 17:25:32 +0800

    add mapreduce

---
+++ src/main/java/com/yuankui/flinkstarted/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,21 @@
+package com.yuankui.flinkstarted.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+    IntWritable one = new IntWritable(1);
+    Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
+}
