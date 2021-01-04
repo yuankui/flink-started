6a1c57eb74f7d687df49bc7b079094d0f5c6ca43
yuankui <yuankui@meituan.com>
2021-01-04 17:05:45 +0800

    init

---
+++ src/main/java/com/yuankui/flinkstarted/FlinkStartedApplication.java
@@ -0,0 +1,13 @@
+package com.yuankui.flinkstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkStartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuankui/flinkstarted/FlinkStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuankui.flinkstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
